3445a1376847d30dc2e65e3d148cfdfc0b793dbd
Wilfried AGO <wilfriedago@pm.me>
2024-08-03 12:19:53 +0100

    chore: add Swagger documentation to API
    This commit adds Swagger documentation to the API using the `@nestjs/swagger` package. The API documentation is generated based on the provided OpenAPI specification using the `DocumentBuilder` class. The Swagger UI is set up at the `/docs` endpoint with custom site title and JSON/YAML document URLs. This addition improves the developer experience by providing a user-friendly interface to explore and test the API endpoints.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableShutdownHooks();
+  app.enableVersioning({ type: VersioningType.URI });
+  app.setGlobalPrefix('api', { exclude: ['/', 'docs'] });
+
+  const config = new DocumentBuilder()
+    .setTitle('API Docs')
+    .setDescription('API Documentation')
+    .setVersion('1.0')
+    .addServer('http://localhost:8080')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'accessToken',
+    )
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'refreshToken',
+    )
+    .addApiKey({ type: 'apiKey', in: 'header', name: 'x-api-key' }, 'apiKey')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document, {
+    customSiteTitle: 'API Docs',
+    jsonDocumentUrl: '/docs/api.json',
+    yamlDocumentUrl: '/docs/api.yaml',
+  });
+
+  await app.listen(8080);
 }
+
 bootstrap();
