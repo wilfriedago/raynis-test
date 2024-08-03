4eda4f440a835db5579e26bc68a79c85539e949c
Wilfried AGO <wilfriedago@pm.me>
2024-08-03 12:21:42 +0100

    feat: add validation and documentation to UpdateTaskDto
    The UpdateTaskDto has been enhanced with validation decorators and Swagger documentation. This ensures that the name and description fields are required and of type string, while the isCompleted field is optional and of type boolean. This improves the reliability and usability of the API.

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,4 +1,30 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
+import { IsBoolean, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+  @ApiProperty({
+    description: 'The name of the task',
+    example: 'Task 1',
+    required: true,
+  })
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    description: 'The description of the task',
+    example: 'Description of Task 1',
+    required: true,
+  })
+  @IsString()
+  description: string;
+
+  @ApiProperty({
+    description: 'The status of the task',
+    example: false,
+    required: false,
+  })
+  @IsBoolean()
+  isCompleted: boolean;
+}
